68ac38cc99926afdc66a62500f09f9b28fc48a5d
firthuns <firthunsnpm@gmail.com>
2026-05-05 11:59:01 +0200

    feat: preparación cometido

--- src/services/hero.service.ts
+++
@@ -1,8 +0,0 @@
-import { heroes } from '../data/heroes.js';
-import type { Hero } from '../data/heroes.js';
-
-
-
-export const findHeroById = ( id: number ) => {
-  return heroes.find( (hero: Hero) => hero.id === id );
-}
